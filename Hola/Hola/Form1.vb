54a9f621b8edb3327b7a611c0c4a1e8753e917d2
AvantisAngel <angel.ramos@avantis.mx>
2015-10-07 14:14:44 -0500

    algo

--- Hola/Hola/Form1.vb
+++ Hola/Hola/Form1.vb
@@ -1,7 +1,7 @@
 ﻿Public Class Form1
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        MsgBox("Realiza cambio en el codigo fuente.")
-        MsgBox("Primera modificación")
+        MsgBox("Hola Mundo")
+        MsgBox("Hola dos")
     End Sub
 End Class
